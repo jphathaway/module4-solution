25cb799d93a840b3d63d3fbc7dc143b0c2c6af6e
jphathaway <68967140+jphathaway@users.noreply.github.com>
2022-01-26 12:37:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function (window)  { //IIFE Wrapper
+  
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].toLowerCase().charAt();
+
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+})(window); // IIFE Wrapper
